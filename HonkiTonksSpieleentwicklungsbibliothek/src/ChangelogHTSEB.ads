66b00c3c4eab2eb0eb80f81c2acefae59bb96422
HonkiTonk <totalplanlos.de@web.de>
2025-02-08 13:24:44 +0100

    README angepasst. Kleinigkeiten korrigiert.

--- HonkiTonksSpieleentwicklungsbibliothek/src/ChangelogHTSEB.ads
+++ HonkiTonksSpieleentwicklungsbibliothek/src/ChangelogHTSEB.ads
@@ -1,8 +1,21 @@
 package ChangelogHTSEB is
    pragma Pure;
    
+   -- Version 0.01.0030 => 0.01. (08.02.2025):
+   
+   --
+   
+   
+
+   -- Version 0.01.0000 => 0.01.0030 (08.02.2025):
+
+   -- README angepasst.
+   -- Kleinigkeiten korrigiert.
+
+
+   
    -- Version 0.01.0000 (08.02.2025):
    
-   -- 
+   -- Erste Veröffentlichung.
 
 end ChangelogHTSEB;
